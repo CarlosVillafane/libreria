b3402b54d7ba5b79a8f794477b504608c0d8705c
CarlosVillafañe <cvillalu@gmail.com>
2021-08-11 23:39:40 -0300

    Cambios en js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-function Libros (autor, titulo, editorial, año){
+let libro = parseInt(promt("Ingresa la cantidad de libros que deseas comprar"))
+/* let compra = prompt("¿Comprar el Caballo de Troya?")
+    if (compra == "si" || compra == "SI" || compra == "Si") {
+        alert("Agregado el " + compra + " Caballo de Troya")
+    } else if (compra = (prompt("¿Comprar la Odisea?"))){
+        alert("Agregado el " + compra + " La Odisea")
+    } else if(compra = prompt("¿Comprar géneros comunicativos?")){
+        alert("Agregado el " + compra + " Géneros comunicativos")
+    } else {
+        alert("¡Gracias!")
+    } */
+
+
+/* function Libros (autor, titulo, editorial, año){
     this.autor = autor;
     this.titulo = titulo;
     this.editorial = editorial;
@@ -6,4 +19,4 @@ function Libros (autor, titulo, editorial, año){
 }
 
 const libros1 = new Libros()
-
+ */

8f70e87e1a00f7c9b31bbed923cf15b6b78bdddb
CarlosVillafañe <cvillalu@gmail.com>
2021-08-11 23:24:17 -0300

    modifiqué js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function Libros (autor, titulo, editorial, año){
+    this.autor = autor;
+    this.titulo = titulo;
+    this.editorial = editorial;
+    this.año = parseInt(año);    
+}
+
+const libros1 = new Libros()
+

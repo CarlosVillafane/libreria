0fd4d532fd4fb41f1ea34ac7e0cd371d395fa5a4
CarlosVillafañe <cvillalu@gmail.com>
2021-08-11 23:46:13 -0300

    Agregue cambios a js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-let libro = parseInt(promt("Ingresa la cantidad de libros que deseas comprar"))
-/* let compra = prompt("¿Comprar el Caballo de Troya?")
+let compra = prompt("¿Comprar el Caballo de Troya?")
     if (compra == "si" || compra == "SI" || compra == "Si") {
-        alert("Agregado el " + compra + " Caballo de Troya")
+        alert("Agregado el libro Caballo de Troya")
     } else if (compra = (prompt("¿Comprar la Odisea?"))){
-        alert("Agregado el " + compra + " La Odisea")
+        alert("Agregado el libro La Odisea")
     } else if(compra = prompt("¿Comprar géneros comunicativos?")){
-        alert("Agregado el " + compra + " Géneros comunicativos")
+        alert("Agregado el libro Géneros comunicativos")
     } else {
         alert("¡Gracias!")
-    } */
-
-
-/* function Libros (autor, titulo, editorial, año){
-    this.autor = autor;
-    this.titulo = titulo;
-    this.editorial = editorial;
-    this.año = parseInt(año);    
-}
-
-const libros1 = new Libros()
- */
+    }
